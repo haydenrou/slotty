b3b7bcae9dd450dccf6daeca4e6887564fa8e68d
Hayden Rouille <hayden@rouille.dev>
2020-06-13 17:31:19 -0400

    Write tests to check for edges of hours
    Change logic to accept integer of minutes

---
+++ lib/slotty/slot.rb
@@ -0,0 +1,33 @@
+require "slotty/timeframe"
+
+module Slotty
+  class Slot
+    attr_reader :range, :begin, :end
+
+    def initialize(range:)
+      @range = range
+      @begin = range.begin
+      @end = range.end
+    end
+
+    def has_overlaps?(excluded_slots = [])
+      return false if excluded_slots.empty?
+
+      excluded_slots.any? do |exc_slot|
+        Timeframe.contains?(exc_slot, range)
+      end
+    end
+
+    def to_s
+      range.begin.strftime('%I:%M %p')
+    end
+
+    def full
+      {
+        start_time: range.begin,
+        end_time:   range.end,
+        time:       to_s
+      }
+    end
+  end
+end
--- spec/slotty_spec.rb
+++ spec/slotty_spec.rb
@@ -1,10 +1,9 @@
 RSpec.describe Slotty do
   it "gets relevant slots" do
     attributes = {
-      from: Time.new(2020, 05, 01, 8, 00),
-      to: Time.new(2020, 05, 01, 9, 30),
-      slot_length: 60 * 60,
-      timeframe: 15 * 60,
+      for_range: Time.new(2020, 05, 01, 8, 00)..Time.new(2020, 05, 01, 9, 30),
+      slot_length_mins: 60,
+      interval_mins: 15,
     }
 
     expected_slots = [
@@ -30,12 +29,11 @@ RSpec.describe Slotty do
 
   it "gets relevant slots without excluded times" do
     attributes = {
-      from: Time.new(2020, 05, 01, 8, 00),
-      to: Time.new(2020, 05, 01, 11, 00),
-      slot_length: 60 * 60,
-      timeframe: 15 * 60,
+      for_range: Time.new(2020, 05, 01, 8, 00)..Time.new(2020, 05, 01, 11, 00),
+      slot_length_mins: 60,
+      interval_mins: 15,
       exclude_times: [
-        { start: Time.new(2020, 05, 01, 9, 01), end: Time.new(2020, 05, 01, 9, 59) },
+        Time.new(2020, 05, 01, 9, 00)..Time.new(2020, 05, 01, 10, 00)
       ]
     }
 
@@ -51,6 +49,101 @@ RSpec.describe Slotty do
         time: "10:00 AM"
       },
     ]
+
+    expect(Slotty.get_slots(attributes)).to eq(expected_slots)
+  end
+
+  it "gets relevant slots without excluded times x2" do
+    attributes = {
+      for_range: Time.new(2020, 05, 01, 8, 00)..Time.new(2020, 05, 01, 18, 00),
+      slot_length_mins: 45,
+      interval_mins: 15,
+      exclude_times: [
+        Time.new(2020, 05, 01, 9, 00)..Time.new(2020, 05, 01, 13, 00),
+        Time.new(2020, 05, 01, 14, 00)..Time.new(2020, 05, 01, 16, 00)
+      ]
+    }
+
+    expected_slots = [
+      {
+        start_time: Time.new(2020, 05, 01, 8, 00),
+        end_time: Time.new(2020, 05, 01, 8, 45),
+        time: "08:00 AM"
+      },
+      {
+        start_time: Time.new(2020, 05, 01, 8, 15),
+        end_time: Time.new(2020, 05, 01, 9, 00),
+        time: "08:15 AM"
+      },
+      {
+        start_time: Time.new(2020, 05, 01, 13, 00),
+        end_time: Time.new(2020, 05, 01, 13, 45),
+        time: "01:00 PM"
+      },
+      {
+        start_time: Time.new(2020, 05, 01, 13, 15),
+        end_time: Time.new(2020, 05, 01, 14, 00),
+        time: "01:15 PM"
+      },
+      {
+        start_time: Time.new(2020, 05, 01, 16, 00),
+        end_time: Time.new(2020, 05, 01, 16, 45),
+        time: "04:00 PM"
+      },
+      {
+        start_time: Time.new(2020, 05, 01, 16, 15),
+        end_time: Time.new(2020, 05, 01, 17, 00),
+        time: "04:15 PM"
+      },
+      {
+        start_time: Time.new(2020, 05, 01, 16, 30),
+        end_time: Time.new(2020, 05, 01, 17, 15),
+        time: "04:30 PM"
+      },
+      {
+        start_time: Time.new(2020, 05, 01, 16, 45),
+        end_time: Time.new(2020, 05, 01, 17, 30),
+        time: "04:45 PM"
+      },
+      {
+        start_time: Time.new(2020, 05, 01, 17, 00),
+        end_time: Time.new(2020, 05, 01, 17, 45),
+        time: "05:00 PM"
+      },
+      {
+        start_time: Time.new(2020, 05, 01, 17, 15),
+        end_time: Time.new(2020, 05, 01, 18, 00),
+        time: "05:15 PM"
+      },
+    ]
+
+    expect(Slotty.get_slots(attributes)).to eq(expected_slots)
+  end
+
+  it "gets no slots" do
+    attributes = {
+      for_range: Time.new(2020, 05, 01, 8, 00)..Time.new(2020, 05, 01, 8, 45),
+      slot_length_mins: 60,
+      interval_mins: 15,
+    }
+
+    expected_slots = []
+
+    expect(Slotty.get_slots(attributes)).to eq(expected_slots)
+  end
+
+  it "gets one slot" do
+    attributes = {
+      for_range: Time.new(2020, 05, 01, 8, 00)..Time.new(2020, 05, 01, 9, 00),
+      slot_length_mins: 60,
+      interval_mins: 15,
+    }
+
+    expected_slots = [{
+      start_time: Time.new(2020, 05, 01, 8, 00),
+      end_time: Time.new(2020, 05, 01, 9, 00),
+      time: "08:00 AM"
+    }]
 
     expect(Slotty.get_slots(attributes)).to eq(expected_slots)
   end
