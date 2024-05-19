5dfb162e1180c0f87a839a270cc53308a6b59765
Hayden Rouille <hayden@rouille.dev>
2024-05-19 16:56:59 +0100

    Add comments for the timeframe class

--- lib/slotty/timeframe.rb
+++ lib/slotty/timeframe.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 module Slotty
+  # The Timeframe class provides methods to determine if one time range
+  # covers or contains another time range.
   class Timeframe
     class << self
+      # Determines if the surrounding_range completely covers the included_range.
+      #
+      # @param surrounding_range [Range] The range that is expected to cover the other.
+      # @param included_range [Range] The range that is expected to be covered.
+      # @return [Boolean] true if surrounding_range covers included_range, otherwise false.
       def covers?(surrounding_range, included_range)
         starts_before = surrounding_range.begin <= included_range.begin
         ends_after = surrounding_range.end >= included_range.end
@@ -10,6 +17,11 @@ module Slotty
         starts_before && ends_after
       end
 
+      # Determines if the potential_slot is contained within the excluder range.
+      #
+      # @param excluder [Range] The range that potentially excludes another.
+      # @param potential_slot [Range] The range that is expected to be contained.
+      # @return [Boolean] true if potential_slot is within excluder, otherwise false.
       def contains?(excluder, potential_slot)
         start_within = potential_slot.begin >= excluder.begin && potential_slot.begin < excluder.end
         end_within = potential_slot.end > excluder.begin && potential_slot.end <= excluder.end
